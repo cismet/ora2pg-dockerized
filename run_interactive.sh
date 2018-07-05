dc5ddc391f313af4f46da793b61d228deacecd1f
p-a-s-c-a-l <pascal@cismet.de>
2018-07-05 10:06:21 +0200

    interactive shell with docker compose

--- run_interactive.sh
+++ run_interactive.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
-## DOCKER-COMPOSE & INTERACTIVE SHELL DOESN'T SEEM TO WORK!
+#docker run -it --rm \
+#	--privileged=true \
+#	--mount type=bind,source="$(pwd)"/export,destination=/export \
+#	--mount type=bind,source="$(pwd)"/ora2pg-client/configuration/ora2pg.conf,destination=/etc/ora2pg/ora2pg.conf \
+#	--name ora2pg-client \
+#	cismet/ora2pg-client:1.0-SNAPSHOT \
+#	/bin/bash
 
-docker run -it --rm \
-	--privileged=true \
-	--mount type=bind,source="$(pwd)"/export,destination=/export \
-	--mount type=bind,source="$(pwd)"/ora2pg-client/configuration/ora2pg.conf,destination=/etc/ora2pg/ora2pg.conf \
-	--name ora2pg-client \
-	cismet/ora2pg-client:1.0-SNAPSHOT \
-	/bin/bash
-
-# DOES NOT WORK: no BIND MOUNTS AVAILABLE	
-#docker-compose run --rm ora2pg-client
+docker-compose run --rm ora2pg-client
